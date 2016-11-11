24c8a7818b41f95e0beeee1566c2647af914e58f
Fran <fsierra@gmail.com>
2016-11-11 19:17:32 +0100

    Modulo Uso camara y Lectura QR. Cambios MainActivity

--- usocamara/src/main/java/com/example/fran/usocamara/MainActivity.java
+++ usocamara/src/main/java/com/example/fran/usocamara/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.fran.usocamara;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,13 +12,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+    private Button scanBtn;
+    private TextView formatTxt, contentTxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Se Instancia el botón de Scan
+        scanBtn = (Button)findViewById(R.id.scan_button);
+        //Se Instancia el Campo de Texto para el nombre del formato de código de barra
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        //Se Instancia el Campo de Texto para el contenido  del código de barra
+        contentTxt = (TextView)findViewById(R.id.scan_content);
+        //Se agrega la clase MainActivity.java como Listener del evento click del botón de Scan
+        scanBtn.setOnClickListener(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,6 +42,36 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        //Se responde al evento click
+        if(view.getId()==R.id.scan_button){
+            //Se instancia un objeto de la clase IntentIntegrator
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            //Se procede con el proceso de scaneo
+            scanIntegrator.initiateScan();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        //Se obtiene el resultado del proceso de scaneo y se parsea
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanningResult != null) {
+            //Quiere decir que se obtuvo resultado pro lo tanto:
+            //Desplegamos en pantalla el contenido del código de barra scaneado
+            String scanContent = scanningResult.getContents();
+            contentTxt.setText("Contenido: " + scanContent);
+            //Desplegamos en pantalla el nombre del formato del código de barra scaneado
+            String scanFormat = scanningResult.getFormatName();
+            formatTxt.setText("Formato: " + scanFormat);
+        }else{
+            //Quiere decir que NO se obtuvo resultado
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No se ha recibido datos del scaneo!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 
     @Override
